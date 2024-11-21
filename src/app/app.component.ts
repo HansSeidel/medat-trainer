321d906b0f0773cd968fcdc7c0e17a2fb28a9c05
HansSeidel <42345214+HansSeidel@users.noreply.github.com>
2024-11-21 12:33:14 +0100

    MEDAT-005: Implemented pseudo-login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {FloatLabelModule} from "primeng/floatlabel";
+import {InputTextModule} from "primeng/inputtext";
+import {Button} from "primeng/button";
+import * as bcrypt from "bcryptjs";
+import {FormsModule} from "@angular/forms";
+import {LandingPageComponent} from "./page/landing-page/landing-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FloatLabelModule, InputTextModule, Button, FormsModule, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private realPassword: string = "$2a$10$VTFbhs1nV2CYwKadwKnhF.8uFNxgQ.AAb/xvbaME3BvNgBuhaBJ62";
   title = 'medat-trainer';
+  password: string;
+  isLoggedIn: boolean | undefined;
+  loginStatus: string;
+
+  constructor() {
+    this.password = '';
+    this.loginStatus = '';
+  }
+
+
+  /**
+   * I know... I know -
+   * But see it that way - I want to have a dummy check so not EVERYONE without a password can enter.
+   * So feel free to lock around when you cracked it ;)
+   */
+  dummyLogin() {
+    this.isLoggedIn = bcrypt.compareSync(this.password, this.realPassword);
+  }
+
+  isInvalid() {
+    return this.isLoggedIn != undefined && !this.isLoggedIn;
+  }
 }
