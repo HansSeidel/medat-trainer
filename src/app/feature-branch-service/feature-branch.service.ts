fc50ed9bcebc09dce2028f762fc188b351c0f65e
HansSeidel <42345214+HansSeidel@users.noreply.github.com>
2024-11-29 20:52:28 +0100

    MEDAT-007.1:Setting master logged in default value to false

---
+++ src/app/feature-branch-service/feature-branch.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeatureBranchService {
+
+  constructor() { }
+
+  getIsLoggedInByDefault(): boolean {
+    return false;
+  }
+}
